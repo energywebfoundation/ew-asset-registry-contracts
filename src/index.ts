d4c8eb84381c8e60d2cb0cbfd66429c0234decfd
Martin Küchler <mkuechle@hs-mittweida.de>
2018-09-26 17:56:40 +0200

    added export functions

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { AssetConsumingRegistryLogic } from './wrappedContracts/AssetConsumingRegistryLogic';
+export { AssetProducingRegistryLogic } from './wrappedContracts/AssetProducingRegistryLogic';
+export { AssetContractLookup } from './wrappedContracts/AssetContractLookup';
+export { migrateAssetRegistryContracts } from './utils/migrateContracts';
